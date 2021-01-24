cd83f4771edb491ec19986087153d27dc9ac07f4
arynoviyanto <arynoviyanto@gmail.com>
2021-01-25 00:45:31 +1300

    make it as a main program

--- main.py
+++ main.py
@@ -9,17 +9,22 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import os
+import sys
 
 def get_refined_matches(matches, kp_1, kp_2):
     matchedKeypoints = []
 
     n = len(matches)
-    diff = np.zeros(n)
+    if (n < 1):
+        return None
 
-    nbin = 5
+    diff = np.zeros(n)
+    num_bin_percentage = 0.1
+    nbin = int(np.ceil(num_bin_percentage * n))
     truth_bin = []
 
-    t = 12 # maximum different orientation in degree
+    t = 4  # maximum different orientation in degree
     agreement_rate = 0.5 # 50% from all matched keypoints
 
     # generate orientation difference array
@@ -31,7 +36,7 @@ def get_refined_matches(matches, kp_1, kp_2):
             diff[i] = 360.0 - diff[i]
 
     for i in range(n):
-        if (len(truth_bin) < 5):
+        if (len(truth_bin) < nbin):
             numT = 0
             for j in range(n):
                 if (j != i):
@@ -42,13 +47,13 @@ def get_refined_matches(matches, kp_1, kp_2):
 
             if (numT >= agreement_rate * n):
                 truth_bin.append(diff[i])
-                matchedKeypoints.append(matches[i])
+                # matchedKeypoints.append(matches[i])
 
         else:
             flag = True
             for j in range(nbin):
                 if (flag):
-                    flag = flag and (abs(truth_bin[j]-diff[i]) < t)
+                    flag = flag and (np.abs(truth_bin[j]-diff[i]) < t)
                 else:
                     break
 
@@ -57,35 +62,45 @@ def get_refined_matches(matches, kp_1, kp_2):
         
     return matchedKeypoints
 
+def get_matched_keypoints(filename_1, filename_2, dir='images'):
 
-img_1 = cv2.imread('images/23_0001.jpg')
-#img_2 = cv2.imread('images/23_0002.jpg')
-img_2 = cv2.imread('images/24_0012.jpg')
+    file_path_1 = os.path.join(dir, filename_1)
+    img_1 = cv2.imread(file_path_1)
+    file_path_2 = os.path.join(dir, filename_2)
+    img_2 = cv2.imread(file_path_2)
 
-gray_1 = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
-gray_2 = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
+    gray_1 = cv2.cvtColor(img_1, cv2.COLOR_BGR2GRAY)
+    gray_2 = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
 
-sift = cv2.SIFT_create()
+    sift = cv2.SIFT_create()
 
-kp_1, des_1 = sift.detectAndCompute(gray_1, None)
-kp_2, des_2 = sift.detectAndCompute(gray_2, None)
+    kp_1, des_1 = sift.detectAndCompute(gray_1, None)
+    kp_2, des_2 = sift.detectAndCompute(gray_2, None)
 
-matcher = cv2.BFMatcher()
-matches = matcher.knnMatch(des_1, des_2, k=2)
+    matcher = cv2.BFMatcher()
+    matches = matcher.knnMatch(des_1, des_2, k=2)
 
-ratio_treshold = 0.75 # 75% between first and second matched keypoints' distances
-valid_matches = []
-for k_1, k_2 in matches:
-    if k_1.distance < 0.75 * k_2.distance:
-        valid_matches.append([k_1])
+    ratio_treshold = 0.75 # 75% between first and second matched keypoints' distances
+    valid_matches = []
+    for k_1, k_2 in matches:
+        if k_1.distance < ratio_treshold * k_2.distance:
+            valid_matches.append([k_1])
 
-matched_img = cv2.drawMatchesKnn(img_1, kp_1, img_2, kp_2, valid_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(matched_img)
-plt.title('Matched keypoints')
-plt.show()
+    return valid_matches, img_1, img_2, kp_1, kp_2
 
-refiened_valid_matches = get_refined_matches(valid_matches, kp_1, kp_2)
-matched_img = cv2.drawMatchesKnn(img_1, kp_1, img_2, kp_2, refiened_valid_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(matched_img)
-plt.title('Matched keypoints after refinement')
-plt.show()
+if __name__ == '__main__':
+    filename_1 = sys.argv[1]
+    filename_2 = sys.argv[2]
+
+    valid_matches, img_1, img_2, kp_1, kp_2 = get_matched_keypoints(filename_1, filename_2)
+
+    matched_img = cv2.drawMatchesKnn(img_1, kp_1, img_2, kp_2, valid_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    plt.imshow(matched_img)
+    plt.title('Matched keypoints:' + str(len(valid_matches)))
+    plt.show()
+
+    refiened_valid_matches = get_refined_matches(valid_matches, kp_1, kp_2)
+    matched_img = cv2.drawMatchesKnn(img_1, kp_1, img_2, kp_2, refiened_valid_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    plt.imshow(matched_img)
+    plt.title('Matched keypoints after refinement: ' + str(len(refiened_valid_matches)))
+    plt.show()
